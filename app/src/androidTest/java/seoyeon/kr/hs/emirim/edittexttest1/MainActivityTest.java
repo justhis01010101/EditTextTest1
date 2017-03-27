ef83f27218f8ea86b9d5c98cc9710817b1073c67
justhis01010101 <1310@gmail.com>
2017-03-27 19:08:13 +0900

    #2 Java Event complete

---
+++ app/src/androidTest/java/seoyeon/kr/hs/emirim/edittexttest1/MainActivityTest.java
@@ -0,0 +1,10 @@
+package seoyeon.kr.hs.emirim.edittexttest1;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by appcreate02 on 2017. 3. 27..
+ */
+public class MainActivityTest {
+
+}
---
+++ app/src/main/java/seoyeon/kr/hs/emirim/edittexttest1/Button.java
@@ -0,0 +1,8 @@
+package seoyeon.kr.hs.emirim.edittexttest1;
+
+/**
+ * Created by appcreate02 on 2017. 3. 27..
+ */
+
+class Button {
+}
